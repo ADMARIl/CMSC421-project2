cef6bd336761bfa2642721133e7af49b8ea0e8bf
Andrew <aings1@umbc.edu>
2019-12-06 23:58:09 -0500

    Added run userland program

--- proj2/userland/sbx421_run.c
+++ proj2/userland/sbx421_run.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <zconf.h>
 #include <errno.h>
 #include <linux/kernel.h>
@@ -19,6 +20,7 @@
 #include <time.h>
 #include <string.h>
 #include <stdbool.h>
+#include <pwd.h>
 
 
 #define sys_sbx421_block 434
@@ -34,6 +36,37 @@ int main(int argc, char *argv[]) {
     if (uid > 0 || euid > 0) {
         return -EPERM;
     }
+
+    // used the command line args
+    char* file = argv[2];
+    char* cmd = argv[3];
+
+    FILE *dataFile = fopen(file, "r");
+    // check to see if there was anything in the file
+    if (dataFile == NULL)
+        return -ENOENT;
+    // make the char arrays to hold the incomming file data
+    char pids [447];
+    char sysIDs [447];
+
+    // get all the data using fscanf
+    fscanf(dataFile,"%446s",pids);
+
+    // loop over the file data until we reach the end of file marker
+    while(fscanf(dataFile, "%446s", pids) != EOF) {
+        scanf((const char *) dataFile, "%s", sysIDs);
+        // get the pid
+        pid_t pid = atol(pids);
+        long sysNum = atol(sysIDs);
+        syscall(sys_sbx421_block, pid, sysIDs);
+        //fscanf(dataFile, "%s", pids);
+    }
+    // closing the file because CMSC 201 said that is good practice
+    fclose(dataFile);
+
+
+    struct passwd *usr = getpwnam(argv[1]);
+
 
 
     return 0;
